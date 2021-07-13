ce767bf08d412d6e4548bde169dcc06d3cc1ea09
mocheer <mocheer@foxmail.com>
2021-07-13 18:48:14 +0800

    update

--- alg/grid.go
+++ alg/grid.go
@@ -1,6 +1,6 @@
 package alg
 
-// GetGridAround 获取网格点周围的8个方格
-func GetGridAround(p [2]float64, size float64) ([2]float64, [2]float64, [2]float64, [2]float64, [2]float64, [2]float64, [2]float64, [2]float64) {
+// GetAroundGrids 获取网格点周围的8个方格
+func GetAroundGrids(p [2]float64, size float64) ([2]float64, [2]float64, [2]float64, [2]float64, [2]float64, [2]float64, [2]float64, [2]float64) {
 	return [2]float64{p[0] + size, p[1]}, [2]float64{p[0], p[1] + size}, [2]float64{p[0] + size, p[1] + size}, [2]float64{p[0] - size, p[1]}, [2]float64{p[0], p[1] - size}, [2]float64{p[0] - size, p[1] - size}, [2]float64{p[0] + size, p[1] - size}, [2]float64{p[0] - size, p[1] + size}
 }
