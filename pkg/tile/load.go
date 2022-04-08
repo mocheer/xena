0de5968825a18276ec512fc772fe60f19fe5a964
郭延宝 <gyb@strongsoft.net>
2022-04-08 08:20:13 +0800

    update

--- pkg/tile/load.go
+++ pkg/tile/load.go
@@ -8,6 +8,8 @@ import (
 	"github.com/mocheer/pluto/pkg/ds"
 	"github.com/mocheer/pluto/pkg/fn"
 	"github.com/mocheer/pluto/pkg/rh"
+	"github.com/mocheer/xena/pkg/gm"
+	"github.com/mocheer/xena/pkg/tile_arcgis"
 )
 
 type LoadConfig struct {
@@ -16,22 +18,60 @@ type LoadConfig struct {
 	MaxZoom int
 }
 
-// Load 下载瓦片服务中的所有瓦片数据
-func Load(remoteURL, dirName string) error {
-	for i := 0; i < 18; i++ {
-		loadingByZoom(remoteURL, i, dirName)
+// LoadArcgis 下载arcgis本地图包中的所有瓦片数据
+func LoadArcgis(localPath, dirName string) error {
+	service, err := tile_arcgis.NewTileArcgis(filepath.Join(localPath, "conf.xml"))
+	if err != nil {
+		return err
 	}
+
+	for z := 0; z < 18; z++ {
+		size := int(math.Pow(2, float64(z)))
+		for i := 0; i < size; i++ {
+			for j := 0; j < size; j++ {
+				zoomRowColumn := fmt.Sprintf("%d/%d/%d", z, i, j)
+				dirPath := filepath.Join(dirName, zoomRowColumn)
+				data, _ := service.ReadTile(gm.Tile{
+					Z: z, X: i, Y: j,
+				})
+				if len(data) != 0 {
+					if !ds.IsExist(dirPath) {
+						ds.Save(dirPath, data)
+						msg := fmt.Sprintf("下载瓦片成功：%s", zoomRowColumn)
+						fmt.Println(msg)
+					}
+				} else {
+					msg := fmt.Sprintf("瓦片为空：%s", zoomRowColumn)
+					fmt.Println(msg)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+// Load 下载瓦片服务中的所有瓦片数据
+func Load(remoteURL, dirName string, maxZoom int) error {
+	fns := []func(){}
+	for i := 0; i < maxZoom; i++ {
+		fns = append(fns, func(i int) func() {
+			return func() {
+				loadingByZoom(remoteURL, i, dirName)
+			}
+		}(i))
+	}
+	fn.GoFns(1, fns).Wait()
 	return nil
 }
 
 // loadingByZoom 下载tile
 func loadingByZoom(rootUrl string, zoom int, dirName string) error {
 	size := int(math.Pow(2, float64(zoom)))
-	maxErrorCount := 10
 	for i := 0; i < size; i++ {
 		for j := 0; j < size; j++ {
 			zoomRowColumn := fmt.Sprintf("%d/%d/%d", zoom, i, j)
-			url := fn.FmtString(rootUrl, map[string]interface{}{"z": zoom, "x": i, "y": j})
+			url := fn.FmtString(rootUrl, map[string]any{"z": zoom, "x": i, "y": j})
 			dirPath := filepath.Join(dirName, zoomRowColumn)
 			if !ds.IsExist(dirPath) {
 				data, err := rh.Get(url)
@@ -44,13 +84,8 @@ func loadingByZoom(rootUrl string, zoom int, dirName string) error {
 				} else {
 					msg := fmt.Sprintf("下载瓦片失败：%s", err)
 					fmt.Println(msg)
-					maxErrorCount--
-					if maxErrorCount < 0 {
-						return nil
-					}
 				}
 			}
-
 		}
 	}
 	return nil
---
+++ pkg/tile_arcgis/constants.go
@@ -0,0 +1,13 @@
+package tile_arcgis
+
+// BundlxMaxidx 紧凑型切片v2
+const BundlxMaxidx = 128
+
+// CompactCacheHeaderLength 紧凑型切片v2头信息长度
+const CompactCacheHeaderLength = 64
+
+// EsriMapCacheStorageModeCompact 紧凑型切片
+const EsriMapCacheStorageModeCompact = "esriMapCacheStorageModeCompact"
+
+// EsriMapCacheStorageModeCompactV2 紧凑型切片V2版本
+const EsriMapCacheStorageModeCompactV2 = "esriMapCacheStorageModeCompactV2"
